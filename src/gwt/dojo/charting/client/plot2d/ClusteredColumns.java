b329e6d7f1502e40d19db5346493be1e107b5550
georgopoulos.georgios <georgopoulos.georgios@gmail.com>
2012-06-01 21:41:43 +0000

    Added dojo.fx.easings module & classes for animated chart demo.

---
+++ src/gwt/dojo/charting/client/plot2d/ClusteredColumns.java
@@ -0,0 +1,16 @@
+package gwt.dojo.charting.client.plot2d;
+
+import gwt.dojo.client.Dojo;
+import gwt.dojo.client.util.JsObject;
+
+public class ClusteredColumns extends Columns {
+	public static final String MODULE = "dojox/charting/plot2d/ClusteredColumns";
+
+	public static JsObject ref() {
+		return Dojo.require(MODULE);
+	};
+
+	protected ClusteredColumns() {
+	}
+
+}
---
+++ src/gwt/dojo/charting/client/themes/Tufte.java
@@ -0,0 +1,15 @@
+package gwt.dojo.charting.client.themes;
+
+import gwt.dojo.charting.client.Theme;
+import gwt.dojo.client.Dojo;
+
+public class Tufte extends Theme {
+	public static final String MODULE = "dojox/charting/themes/Tufte";
+
+	public static Wetland ref() {
+		return Dojo.require(MODULE).cast();
+	}
+
+	protected Tufte() {
+	}
+}
