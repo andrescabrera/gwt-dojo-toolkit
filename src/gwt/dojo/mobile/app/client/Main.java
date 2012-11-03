286d5c3182c8b861b7f1f22f395c5015c74da4f3
georgopoulos.georgios <georgopoulos.georgios@gmail.com>
2012-11-03 00:38:43 +0000

    Simple Dojo demo

--- src/gwt/dojo/mobile/app/client/Main.java
+++ src/gwt/dojo/mobile/app/client/Main.java
@@ -1,12 +1,19 @@
 package gwt.dojo.mobile.app.client;
 
 import gwt.dojo.client.Dojo;
+import gwt.dojo.client.MessageHub;
 import gwt.dojo.client.RequireCallback;
+import gwt.dojo.client.SubscribeCallback;
 import gwt.dojo.client.util.JsArray;
 import gwt.dojo.client.util.JsObject;
 import gwt.dojo.mobile.client.Parser;
+import gwt.dojo.mobile.client.View;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -25,7 +32,26 @@ public class Main implements EntryPoint, RequireCallback {
 
 	@Override
 	public void callback(JsObject arguments) {
+		MessageHub.subscribe(View.TOPIC_STARTVIEW, new SubscribeCallback() {
+			@Override
+			public void callback(String topic, JsArray message) {
+				hideLoadDiv();
+			}
+		});
+
 		// Parse the page for widgets!
-		Parser.ref().parse();
+		Parser.parse();
+	}
+
+	protected void hideLoadDiv() {
+		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+			@Override
+			public void execute() {
+				Element loadDiv = Document.get().getElementById("loadDiv");
+				if (loadDiv != null) {
+					loadDiv.removeFromParent();
+				}
+			}
+		});
 	}
 }
